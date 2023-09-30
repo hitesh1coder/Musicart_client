cdc452c2715d4e9b3c60d135fb21833ba190e7ee
codewitHK1 <kumarhk1002@gmail.com>
2023-09-30 11:08:37 +0530

    last commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,11 @@ import "./App.css";
 import { Provider } from "react-redux";
 
 import store from "./redux/store.js";
-import DemoCarousel from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <DemoCarousel />
+      <App />
     </Provider>
   </React.StrictMode>
 );
