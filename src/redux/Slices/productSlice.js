eff257b9fa01bc6fcb08ad14c98442bbdf419428
hitesh1coder <124659378+hitesh1coder@users.noreply.github.com>
2023-11-27 11:01:04 +0530

    Update productSlice.js

--- src/redux/Slices/productSlice.js
+++ src/redux/Slices/productSlice.js
@@ -36,7 +36,7 @@ export const filterProducts = createAsyncThunk(
     try {
       const response = await axios.get(
         `${import.meta.env.VITE_SERVER_HOST}/products/api/filter`,
-        { params: filters }
+        { params: filters["0"] }
       );
 
       return response.data;
