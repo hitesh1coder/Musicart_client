eeff6cdb497b9296fd01b2367ff0cca7804e6363
codewitHK1 <kumarhk1002@gmail.com>
2023-12-01 11:05:00 +0530

    fix mobile header styles and loading skeletan styles

--- src/MobileComponents/MobileHeader/MobileHeader.jsx
+++ src/MobileComponents/MobileHeader/MobileHeader.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import styles from "./MobileHeader.module.css";
-import { SearchBar } from "../../Components/index";
 import logo from "/images/musicartLogo.png";
+import { useSelector } from "react-redux";
+import usersIcon from "/images/icons8-group-32.png";
 
 const MobileHeader = () => {
+  const { user } = useSelector((state) => state.auth);
   return (
     <>
       <div className={styles.header_container}>
         <div className={styles.header_div}>
           <img className={styles.logo_img} src={logo} alt="logo" />
-          {/* <SearchBar /> */}
+          <div className={styles.user_info}>
+            <img src={usersIcon} alt="user" />
+            <h3>{user?.name.toUpperCase()}</h3>
+          </div>
         </div>
       </div>
     </>
